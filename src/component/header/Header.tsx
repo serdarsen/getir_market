78264335c393a736511cd0adf6b0d62b6ce6af8c
serdarsen <serdarsendev@gmail.com>
2022-09-03 07:25:11 +0300

    Change header layout to grid.

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import BasketIcon from "../../asset/BasketIcon";
 import LogoIcon from "../../asset/LogoIcon";
 import "./header.scss";
 
-const Header: React.FC = () => (
-  <div className="app__header">
-    <div className="app__header_logo">
-      <LogoIcon />
+const Header: React.FC = () => {
+  const [toggleBasket, setToggleBasket] = useState(false);
+
+  return (
+    <div className="app__header">
+      <div className="app__header_logo">
+        <LogoIcon />
+      </div>
+      <div
+        className="app__header_basket"
+        onClick={() => setToggleBasket(!toggleBasket)}
+        onKeyDown={() => setToggleBasket(!toggleBasket)}
+        onBlur={() => setToggleBasket(false)}
+        role="button"
+        tabIndex={0}
+      >
+        <BasketIcon />
+        <p>₺ 39,97</p>
+        {toggleBasket
+        && (
+        <div
+          className="app__header_basket-container"
+        >
+          <div className="app__header_basket-items" />
+        </div>
+        )}
+      </div>
     </div>
-    <div className="app__header_basket">
-      <BasketIcon />
-      <p>₺ 39,97</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;

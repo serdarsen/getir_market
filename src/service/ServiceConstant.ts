c20ff085572140207594e5a0f37fa57ea2d7ee4a
serdarsen <serdarsendev@gmail.com>
2022-09-04 17:37:42 +0300

    Fix json api url.

--- src/service/ServiceConstant.ts
+++ src/service/ServiceConstant.ts
@@ -1,4 +1,1 @@
-const port = 8080;
-const isPro = process.env.npm_lifecycle_event === "build";
-
-export const JSON_API = isPro ? "api" : `http://localhost:${port}/api`;
+export const JSON_API = "api";

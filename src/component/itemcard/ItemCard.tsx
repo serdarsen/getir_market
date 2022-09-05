e00a8f6be6deb66371ca8c18d88822afed3610d6
serdarsen <serdarsendev@gmail.com>
2022-09-05 06:58:42 +0300

    Add item card content.

--- src/component/itemcard/ItemCard.tsx
+++ src/component/itemcard/ItemCard.tsx
@@ -8,7 +8,21 @@ type Prop = {
 
 const ItemCard: React.FC<Prop> = ({ item }) => (
   <div className="app__item-card">
-    {item.name}
+    <div className="app__item-bg">
+      <img
+        src="//www.colorbook.io/imagecreator.php?hex=C4C4C4&width=92&height=92&text=Item"
+        alt={item.name}
+      />
+    </div>
+    <div className="app_item-typography">
+      <p className="app__item-price">
+        ₺
+        {" "}
+        {item.price}
+      </p>
+      <p className="app__item-name">{item.name}</p>
+    </div>
+    <button type="button" className="app__item-button">Add</button>
   </div>
 );
 

866d16da8d2e36e035b27dd066226ba7b23cbf4c
serdarsen <serdarsendev@gmail.com>
2022-09-08 15:37:30 +0300

    Add ability to change page by pagination. Fix style priorities.

--- src/service/ItemSaga.ts
+++ src/service/ItemSaga.ts
@@ -1,16 +1,22 @@
 import {
-  call, CallEffect, ForkEffect, put, PutEffect, takeEvery,
+  call, CallEffect, ForkEffect, put, PutEffect, select, SelectEffect, takeEvery,
 } from "redux-saga/effects";
 import { findItems } from "./ItemService";
 import { findItemsSuccess } from "./ItemSlice";
+import { RootState } from "./ReduxStore";
 
-function* findItemsFetch(): Generator<CallEffect | PutEffect, void> {
-  const items = yield call(findItems);
+const getPageNo = (state: RootState): number => state.pageNo.pageNo;
+
+function* findItemsFetch(): Generator<
+SelectEffect | CallEffect | PutEffect, void> {
+  const pageNo = yield select(getPageNo);
+  const items = yield call(findItems, [pageNo]);
   yield put(findItemsSuccess(items));
 }
 
 function* ItemSaga(): Generator<ForkEffect, void> {
   yield takeEvery("ItemSlice/findItemsFetch", findItemsFetch);
+  yield takeEvery("PaginationSlice/setPageNo", findItemsFetch);
 }
 
 export default ItemSaga;
--- src/service/ItemService.ts
+++ src/service/ItemService.ts
@@ -1,8 +1,13 @@
 import { Item, Pageable } from "../model";
 import BaseService from "./BaseService";
 
-export const findItems = async (): Promise<Pageable<Item[]>> => {
-  const response = await BaseService.get("items?_page=109&_limit=16");
+export const findItems = async (options: any[]):
+Promise<Pageable<Item[]>> => {
+  const [pageNo] = options;
+
+  const response = await BaseService.get(
+    `items?_page=${pageNo}&_limit=16`,
+  );
 
   const { data, headers = {} } = response || {};
   const totalCount = parseInt(headers["x-total-count"] || "0", 10);

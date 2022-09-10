09626de8ae2d3fd8c0dbbe4c8d1562ec154bb9c6
serdarsen <serdarsendev@gmail.com>
2022-09-10 21:16:26 +0300

    Remove debug log.

--- src/service/BaseService.ts
+++ src/service/BaseService.ts
@@ -8,13 +8,6 @@ if (isDev) {
   axios.defaults.baseURL = `${protocol}//${hostname}:3004`;
 }
 
-console.log(
-  "myDebug process.env.npm_lifecycle_event: ",
-  process.env.npm_lifecycle_event,
-);
-console.log("myDebug isDev: ", isDev);
-console.log("myDebug axios.defaults.baseURL: ", axios.defaults.baseURL);
-
 export const PRODUCT_PLACEHOLDER_IMAGE_URL = "//www.colorbook.io/imagecreator.php?hex=C4C4C4&width=92&height=92&text=Item";
 
 export const JSON_API_URL = "/api";
